8a9c18b26eb8db4039f149cf345f13720e6b8e07
Turbo <619669453@qq.com>
2020-09-30 11:16:05 +0800

    登录和注册按钮的选中切换

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div id="login">
-    这里是登录页
-    <el-button type="primary">主要按钮</el-button>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li  v-for="item in menuTab" :key="item.id" :class="{'current':item.current}" @click="toggleMenu(item)">{{item.txt}}</li>
+
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+    name:'login',
+    data(){
+      return{
+        menuTab: [
+          { txt:'登录',current:true},
+          { txt:'注册',current:false}
+        ],
+      }
+    },
+    created(){},
+    mounted(){},
+    methods:{
+      toggleMenu(data){
+        this.menuTab.forEach(element => {
+          element.current=false
+        });
+        data.current=true
+      }
+    }
 
 }
 </script>
@@ -15,5 +38,24 @@ export default {
 #login{
   height:100vh;
   background-color: #344a5f;
+}
+.login-wrap{
+  width:330px;
+  margin: auto;
+}
+.menu-tab{
+  text-align: center;
+  li{
+    display: inline-block;
+    width:88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: white;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+  .current{
+    background-color: rgba(0,0,0,.1);
+  }
 }
 </style>
